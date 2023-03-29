a1eb192bd1ce02bdf6c0e2ca2b323da0f21c6a9c
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2023-03-29 10:56:12 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/stopwatch-vue/',
   css: {
     preprocessorOptions: {
       scss: {
